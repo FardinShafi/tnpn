a5ced634178ff74be783cb334b412b5f2c405ce9
NITRO\fardin <fardin467@gmail.com>
2024-05-14 23:08:03 +0600

    basic home

--- src/client/.next/static/chunks/webpack.js
+++ src/client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ff38d8a05735de7"; }
+/******/ 		__webpack_require__.h = function() { return "ec40606f2967d94f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,5 +1,15 @@
-export default function Home () {
-    return (
-        <h1>Hi</h1>
-    );
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Homepage!</h1>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+      <Link to="/signup">
+        <button>Signup</button>
+      </Link>
+    </div>
+  );
 }
